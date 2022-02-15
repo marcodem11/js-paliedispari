0b79930fc42c80487cb66ece62b19bdc8944114c
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-15 10:17:31 +0100

    Parola Palindroma

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('Pali e Dispari')
+
+const gerry = prompt('Inserisci parola');
+console.log(gerry);
+
+if(ePalindroma(gerry) ) {
+    console.log('La parola è palindroma');
+} else {
+    console.log('La parola non è palindroma');
+}
+
+function invertiParola( parola){
+    const chars = parola.split('');
+    const rejoin = chars.reverse().join('');
+    return rejoin
+}
+
+function ePalindroma( parola) {
+    const invertita = invertiParola(parola);
+    if(parola === invertita) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
